9b4c82418a0209091f7a9b33280b5c314e4bdbf0
Richie_Sjt <richi252012@hotmail.com>
2021-04-22 18:41:09 -0500

    Home webpage

--- src/routers/main-routes.js
+++ src/routers/main-routes.js
@@ -14,6 +14,30 @@ router.get('/home', (req, res) => {
     })
 })
 
+router.get('/nosotros', (req, res) => {
+    res.render('nosotros', {
+        active: { nosotros: true }
+    })
+})
+
+router.get('/catalogo', (req, res) => {
+    res.render('catalogo', {
+        active: { catalogo: true }
+    })
+})
+
+router.get('/designs', (req, res) => {
+    res.render('designs', {
+        active: { designs: true }
+    })
+})
+
+router.get('/contactanos', (req, res) => {
+    res.render('contactanos', {
+        active: { contactanos: true }
+    })
+})
+
 // 404
 router.get('*', (req, res) => {
     res.render('404',  {
